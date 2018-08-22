50499a87623d502def2579a25c62f6fbb59edf13
kuhlin <kuhlin@gmail.com>
2018-08-22 18:49:33 +0200

    Cleaning up my mess

---
+++ PatientData/PatientData/App_Start/MongoConfig.cs
@@ -0,0 +1,35 @@
+﻿using MongoDB.Driver;
+using PatientData.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PatientData
+{
+    public static class MongoConfig
+    {
+        public static void Seed()
+        {
+            var patients = PatientDb.Open();
+            if (!patients.AsQueryable().Any(p => p.Name == "Scott"))
+            {
+                var data = new List<Patient>()
+                {
+                    new Patient {Name = "Scott",
+                                 Ailments = new List<Ailment>(){new Ailment { Name = "Crazy"}, new Ailment { Name="Old"}},
+                                 Medications = new  List<Medication>(){new Medication {Name = "Aspirin", Doses=2}, new Medication {Name="Paracetamol", Doses =1 } }
+                    },
+                    new Patient {Name = "Joy",
+                                 Ailments = new List<Ailment>(){new Ailment { Name = "Crazy"}, new Ailment { Name="Old"}},
+                                 Medications = new  List<Medication>(){new Medication {Name = "Aspirin", Doses=2}, new Medication {Name="Paracetamol", Doses =1 } }
+                    },
+                    new Patient {Name = "Sarah",
+                                 Ailments = new List<Ailment>(){new Ailment { Name = "Crazy"}, new Ailment { Name="Old"}},
+                                 Medications = new  List<Medication>(){new Medication {Name = "Aspirin", Doses=2}, new Medication {Name="Paracetamol", Doses =1 } }
+                    }
+                };
+
+                patients.InsertMany(data);
+            }
+        }
+    }
+}
---
+++ PatientData/PatientData/Models/PatinetDb.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Driver;
+
+namespace PatientData.Models
+{
+    public static class PatientDb
+    {
+        public static IMongoCollection<Patient> Open()
+        {
+            var client = new MongoClient("mongodb://kuhlin:<PASSWORD>@cluster0-shard-00-00-zo3ut.mongodb.net:27017,cluster0-shard-00-01-zo3ut.mongodb.net:27017,cluster0-shard-00-02-zo3ut.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true");
+            //var client = new MongoClient("mongodb + srv://kuhlin:<PASSWORD>@cluster0-zo3ut.mongodb.net/test?retryWrites=true");
+
+            var db = client.GetDatabase("PatientDb");
+            return db.GetCollection<Patient>("Patients");
+        }
+    }
+}
