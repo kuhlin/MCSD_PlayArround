1b183c5f0d228980fdabf8e6fe88803268c5e18b
kuhlin <kuhlin@gmail.com>
2018-08-23 18:29:36 +0200

    FaulTracker

---
+++ FailTracker/FailTracker/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using FailTracker.Web2.Data;
+using System.Web.Mvc;
+
+namespace FailTracker.Web2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ApplicationDbContext _contex;
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ FailTracker/FailTracker/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using FailTracker.Web2.Domain;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace FailTracker.Web2.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection")
+        {
+        }
+    }
+}
