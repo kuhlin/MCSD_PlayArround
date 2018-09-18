c836406b42f5d9fce054fbda0640a21e20983596
kuhlin <kuhlin@gmail.com>
2018-09-18 20:29:26 +0200

    Puting the API up to date

--- Library.API/Library.API/Startup.cs
+++ Library.API/Library.API/Startup.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Library.API.Entities;
+using Library.API.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace Library.API
 {
@@ -24,6 +21,15 @@ namespace Library.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            // register the DbContext on the container, getting the connection string from
+            // appSettings (note: use this during development; in a production environment,
+            // it's better to store the connection string in an environment variable)
+            var connectionString = Configuration["connectionStrings:libraryDBConnectionString"];
+            services.AddDbContext<LibraryContext>(o => o.UseSqlServer(connectionString));
+
+            // register the repository
+            services.AddScoped<ILibraryRepository, LibraryRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
