b820c92ba569ae8fbf121ad5d1962fe25fa712f1
kuhlin <kuhlin@gmail.com>
2018-12-10 18:14:31 +0100

    Falta los controladores y la vistas

--- PhotoSharingApplication/PhotoSharingApplication/Global.asax.cs
+++ PhotoSharingApplication/PhotoSharingApplication/Global.asax.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Data.Entity;
+using PhotoSharingApplication.Models;
 
 namespace PhotoSharingApplication
 {
@@ -16,6 +18,7 @@ namespace PhotoSharingApplication
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            Database.SetInitializer<PhotoSharingContext>(new PhotoSharingInitializer());
         }
     }
 }
---
+++ PhotoSharingApplication/PhotoSharingApplication/Models/Photo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PhotoSharingApplication.Models
+{
+    public class Photo
+    {
+        public int PhotID { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [DisplayName("Picture")]
+        public byte[] PhotoFile { get; set; }        
+        public string ImageMimeType { get; set; }
+        [DataType(DataType.MultilineText)]
+        public string Description { get; set; }
+        [DataType(DataType.DateTime)]
+        [DisplayName("Create Date")]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yy}")]
+        public DateTime CreatedDate { get; set; }
+        public string UserName { get; set; }
+        //relación con los comentarios
+        [DataType(DataType.MultilineText)]
+        public virtual ICollection<Comment>Comments { get; set; }
+    }
+}
