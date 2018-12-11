176ce5da3c02cc043ac9ac99bfacea638d192e24
George <kuhlin@gmail.com>
2018-12-11 17:52:36 +0100

    Cleaning up my mess

--- PatientData/PatientData/Models/PatinetDb.cs
+++ PatientData/PatientData/Models/PatinetDb.cs
@@ -10,7 +10,7 @@ namespace PatientData.Models
     {
         public static IMongoCollection<Patient> Open()
         {
-            var client = new MongoClient("mongodb://kuhlin:MongoDBDragon25@cluster0-shard-00-00-zo3ut.mongodb.net:27017,cluster0-shard-00-01-zo3ut.mongodb.net:27017,cluster0-shard-00-02-zo3ut.mongodb.net:27017/PatientDb.PatientDb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true");
+            var client = new MongoClient("mongodb://kuhlin:<Passworrd>@cluster0-shard-00-00-zo3ut.mongodb.net:27017,cluster0-shard-00-01-zo3ut.mongodb.net:27017,cluster0-shard-00-02-zo3ut.mongodb.net:27017/PatientDb.PatientDb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true");
             //var client = new MongoClient("mongodb + srv://kuhlin:<MongoDB%23Dragon25>@cluster0-zo3ut.mongodb.net/test?retryWrites=true");
 
             var db = client.GetDatabase("PatientDb");
